ef1011b54bc57a4390d3344707d174a9df54941d
Kailash Sankar <ksankar@conviva.com>
2025-07-22 17:42:20 +0530

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../public/css/base.css';
-import '../public/css/vendor.css';
-import '../public/css/main.css';
-import '../public/css/vandm.css';
+import './css/base.css';
+import './css/main.css';
+import './css/vandm.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
